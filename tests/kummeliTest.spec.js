dbd9c171ac8d1644eeaed6697103ced1746a4b1d
Perttu Harvala <harvala.perttu@gmail.com>
2023-04-25 12:48:13 +0300

    kummeli test lambda

--- tests/kummeliTest.spec.js
+++ tests/kummeliTest.spec.js
@@ -1,8 +1,30 @@
 const { test, expect } = require('@playwright/test');
+const { chromium } = require('playwright')
 const AxeBuilder = require('@axe-core/playwright').default;
 import { createHtmlReport } from 'axe-html-reporter';
 
+
 test('Find fifth episode of third season', async ({ page }) => {
+  const capabilities = {
+    'browserName': 'pw-firefox', // Browsers allowed: `Chrome`, `MicrosoftEdge`, `pw-chromium`, `pw-firefox` and `pw-webkit`
+    'browserVersion': 'latest',
+    'LT:Options': {
+      'platform': 'Windows 10',
+      'build': 'Kummeli Test',
+      'name': 'Kummeli Test',
+      'user': "nameHere.",
+      'accessKey': "keyHere",
+      'network': true,
+      'video': true,
+      'console': true,
+      'geoLocation': 'FI'
+    }
+  }
+
+  const browser = await chromium.connect({
+    wsEndpoint: `wss://cdp.lambdatest.com/playwright?capabilities=${encodeURIComponent(JSON.stringify(capabilities))}`
+  })
+  page = await browser.newPage();
   await page.goto('https://areena.yle.fi/1-3339547');
 
   const seasonElement = await page.locator('li.DesktopDropdown_item__QxPNK:nth-child(3) button.AlternateButton_root__5TY0i');
@@ -27,4 +49,5 @@ test('Find fifth episode of third season', async ({ page }) => {
     } catch (e) {
 
     }
-});
+    await browser.close()
+},{timeout: 90000});
